ffac8f6b50376f702d8ad09ad0e76d251fcde8b7
GenaKashin <gengambit37@gmail.com>
2020-10-02 13:26:33 +0300

    Updates

---
+++ precache-manifest.8dc38a24cfde4095b045774233cbb69f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4f973936bcafbff61a3edc82cfb0ab",
+    "url": "/fitness_house/index.html"
+  },
+  {
+    "revision": "0c3ba432ea7026e95cbd",
+    "url": "/fitness_house/static/css/main.b59d7f32.chunk.css"
+  },
+  {
+    "revision": "40f7532b5caae63929ff",
+    "url": "/fitness_house/static/js/2.d5c06b03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fitness_house/static/js/2.d5c06b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c3ba432ea7026e95cbd",
+    "url": "/fitness_house/static/js/main.e242ac47.chunk.js"
+  },
+  {
+    "revision": "ad67393e9e202468edbd",
+    "url": "/fitness_house/static/js/runtime-main.b01464ef.js"
+  }
+]);
